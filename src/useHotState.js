37ff295b407e837835a1ac67d9f533b2494aa54e
Dom Vinyard <domvinyard@me.com>
2019-10-11 19:25:28 +0100

    Update useHotState.js

--- src/useHotState.js
+++ src/useHotState.js
@@ -8,4 +8,21 @@ const useHotState = ({ watch, update, initial }) => {
   return hotState
 }
 
+// used in App.js:
+
+/*
+const bets = useHotState({
+    watch: [cash],
+    update: () =>
+      cash < 50
+        ? [1, 2, 5]
+        : cash < 200
+        ? [10, 20, 50]
+        : cash < 2000
+        ? [50, 100, 250]
+        : [250, 500, 1000],
+    initial: [10, 20, 50]
+  })
+*/
+
 export default useHotState
